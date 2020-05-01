9ac4daefbb45bc5473d58b38d2c38344d8cd2912
Jackson Reinagel <jreinagel4@gmail.com>
2020-05-01 17:39:14 -0600

    Update with package.json, Grunt and babel configurations set-up

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    clean: ['dist/'],
+    browserify: {
+      'dist/js/index.js': ['js/index.js']
+    },
+    rework: {
+      'dist/css/style.css': ['css/style.css'],
+      options: {
+        vendors: ['-moz-', '-webkit-']
+      }
+    },
+    copy: {
+      images: {
+        src: 'img/**',
+        dest: 'dist/',
+        expand: true
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-rework');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'browserify', 'rework', 'copy', 'jshint', 'qunit']);
+
+};
